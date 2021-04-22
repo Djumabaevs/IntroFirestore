560546061792be4148809e03b9ec0ef32a273491
Djumabaevs <djumabaevb@gmail.com>
2021-04-22 20:51:59 +0600

    create database and key/value to add to collection

--- app/src/main/java/com/bignerdranch/android/introfirestore/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/introfirestore/MainActivity.java
@@ -3,12 +3,31 @@ package com.bignerdranch.android.introfirestore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText enterName;
+    private EditText enterThought;
+    private Button saveButton;
+
+    //Keys
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_THOUGHT = "thought";
+
+    //Connection to FireStore
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        saveButton.setOnClickListener(view -> {
+
+        });
+
     }
 }
